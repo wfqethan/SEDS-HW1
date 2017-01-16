e4d82a33d0de3643fcbca31aa41bdc5f2f4e60a5
Ethan Wang <wfqethan@uw.edu>
2017-01-16 14:46:58 -0800

    Added in-line comment

--- FangqiWang_setup.sh
+++ FangqiWang_setup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+# SEDS HW#1 Fangqi Wang 01/16/2017
 
-echo "Would you like to download Harvard Clean Energy Data?(Type 1-Yes/2-No)"
-select option in Yes No
+echo "Would you like to download Harvard Clean Energy Project Database?(Type 1-Yes/2-No)"
+select option in Yes No  # Create a menu for user to select options
 do
 	case $option in
-		Yes )
+		Yes )  # Proceed with folowing command if user would like to download the file
 			echo "Downloading file"
+			# Download the file
 			curl -O http://faculty.washington.edu/dacb/HCEPDB_moldata.zip
 			echo "Unzipping the file"
+    		# Unzip the file
     		unzip HCEPDB_moldata.zip
-    		echo "Finished"
+    		echo "Completed"
     		exit
 			;;
-		No )
+		No ) # Exit the program if user does not want to download the file
 			echo "Exit program"
 			exit
 			;;
