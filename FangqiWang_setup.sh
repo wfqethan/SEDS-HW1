ab514e42e98a54c6291a22a6f28b451e4a736d41
Ethan Wang <wfqethan@uw.edu>
2017-01-15 17:21:27 -0800

    Initial commit

--- FangqiWang_setup.sh
+++ FangqiWang_setup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "making direcoty"
+echo "Would you like to download Harvard Clean Energy Data?(Type 1-Yes/2-No)"
+select option in Yes No
+do
+	case $option in
+		Yes )
+			echo "Downloading file"
+			curl -O http://faculty.washington.edu/dacb/HCEPDB_moldata.zip
+			echo "Unzipping the file"
+    		unzip HCEPDB_moldata.zip
+    		echo "Finished"
+    		exit
+			;;
+		No )
+			echo "Exit program"
+			exit
+			;;
+	esac
+done
